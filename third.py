f2214dfff7de6c6cd6aa88b0a89a2807323873a6
S-U King <eabumar54@gmail.com>
2025-01-13 22:25:25 +0500

    add Dmas rule

--- third.py
+++ third.py
@@ -1,6 +1,9 @@
 num1 = int(input("Enter your number"))
 num2 = int(input("Enter your number"))
+num3 = int(input("Enter your number"))
+num4 = int(input("Enter your number"))
+num5 = int(input("Enter your number"))
 
-sum = num1 + num2
+sum = num1 + num2 - num3 / num4 * num5
 
 print(sum)
